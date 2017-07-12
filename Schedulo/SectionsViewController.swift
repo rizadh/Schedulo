34a6cf3a5add6de7a0814faa7e3745c83c7976fb
Rizadh Nizam <rizadh@users.noreply.github.com>
2017-07-12 15:21:46 -0400

    Support deletion of sections

--- Schedulo/SectionsViewController.swift
+++ Schedulo/SectionsViewController.swift
@@ -19,13 +19,14 @@ class SectionsViewController: UITableViewController {
     private let saveHandler: ([Section]) -> Void
 
     // MARK: - Private Methods
+
     private func addSection() {
         let alertController = UIAlertController(title: "Add Section", message: "Enter a uniquely identifying name for the section. It cannot be blank.", preferredStyle: .alert)
 
         let addAction = UIAlertAction(title: "Add", style: .default, handler: { _ in
-            if let identifier = alertController.textFields?.first?.text, !identifier.isEmpty {
+            if let identifier = alertController.textFields?.first?.text, self.identifierIsValid(identifier) {
                 self.sections.append(Section(identifier: identifier, sessions: []))
-                self.tableView.insertSections([self.sections.count - 1], with: .automatic)
+                self.tableView.insertSections([self.sections.count - 1], with: .left)
             } else {
                 self.addSection()
             }
@@ -45,8 +46,35 @@ class SectionsViewController: UITableViewController {
         self.tableView.deselectRow(at: IndexPath(row: 0, section: self.sections.count), animated: true)
     }
 
-    private func editSession(_ sessionIndex: Int, ofSection sectionIndex: Int) {
+    private func deleteSection(at sectionIndex: Int) {
+        sections.remove(at: sectionIndex)
+        tableView.deleteSections([sectionIndex], with: .left)
+    }
+
+    private func addSession(to sectionIndex: Int) {
         print("Adding session")
+    }
+
+    private func editSession(_ sessionIndex: Int, of sectionIndex: Int) {
+        print("Editing section")
+    }
+
+    private func identifierIsValid(_ identifier: String, for sectionIndexOrNil: Int? = nil) -> Bool {
+        if identifier.isEmpty {
+            return false
+        }
+
+        for (index, section) in self.sections.enumerated() {
+            if let sectionIndex = sectionIndexOrNil, sectionIndex == index {
+                continue
+            }
+
+            if section.identifier == identifier {
+                return false
+            }
+        }
+
+        return true
     }
 
     // MARK: - Initializers
@@ -59,6 +87,8 @@ class SectionsViewController: UITableViewController {
 
         self.navigationItem.title = "Sections"
         self.navigationItem.rightBarButtonItem = editButtonItem
+
+        self.tableView.allowsSelectionDuringEditing = true
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -77,47 +107,47 @@ class SectionsViewController: UITableViewController {
         }
 
         if tableView.isEditing {
-            return sections[section].sessions.count + 2
+            return sections[section].sessions.count + 3
         }
 
         return sections[section].sessions.count + 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        var cell = UITableViewCell()
 
-        switch (indexPath.section, indexPath.row) {
+        let effectiveRow = tableView.isEditing ? indexPath.row - 1 : indexPath.row
+
+        switch (indexPath.section, effectiveRow) {
         case (sections.count, _):
             cell.textLabel!.text = "Add Section"
             cell.textLabel!.textColor = cell.textLabel!.tintColor
             cell.textLabel!.textAlignment = .center
-        case (let section, var row):
-            if tableView.isEditing {
-                row -= 1
-            }
+        case let (section, row) where (0..<sections[section].sessions.count).contains(row):
+            cell.textLabel!.text = sections[section].sessions[row].description
+            cell.accessoryType = .disclosureIndicator
+        case let (section, row) where row == sections[section].sessions.count:
+            cell.textLabel!.text = "Add Session"
+            cell.accessoryType = .disclosureIndicator
+        case (let section, -1) where tableView.isEditing:
+            let originalIdentifier = sections[section].identifier
+            let sectionIdentifierCell = TextFieldCell {
+                let sectionIndex = self.tableView.indexPath(for: cell)!.section
 
-            switch row {
-                case 0..<sections[section].sessions.count:
-                    cell.textLabel!.text = sections[section].sessions[row].description
-                    cell.accessoryType = .disclosureIndicator
-                case sections[section].sessions.count:
-                    cell.textLabel!.text = "Add Session"
-                    cell.accessoryType = .disclosureIndicator
-                case -1:
-                    let originalIdentifier = sections[section].identifier
-                    let sectionIdentifierCell = TextFieldCell {
-                        if $0.isEmpty {
-                            self.sections[section].identifier = originalIdentifier
-                        } else {
-                            self.sections[section].identifier = $0
-                        }
-                    }
-                    sectionIdentifierCell.textField.placeholder = "Identifier"
-                    sectionIdentifierCell.textField.text = originalIdentifier
-                    return sectionIdentifierCell
-                default:
-                    break
+                if self.identifierIsValid($0, for: sectionIndex) {
+                    self.sections[sectionIndex].identifier = $0
+                } else {
+                    self.sections[sectionIndex].identifier = originalIdentifier
+                }
             }
+            sectionIdentifierCell.textField.placeholder = originalIdentifier
+            sectionIdentifierCell.textField.text = originalIdentifier
+            cell = sectionIdentifierCell
+        case let (section, row) where row == sections[section].sessions.count + 1 && tableView.isEditing:
+            cell.textLabel!.text = "Delete Section"
+            cell.textLabel!.textColor = .red
+        default:
+            break
         }
 
         return cell
@@ -127,8 +157,12 @@ class SectionsViewController: UITableViewController {
         switch (indexPath.section, indexPath.row) {
         case (sections.count, _):
             addSection()
-        case (let section, let row):
-            editSession(row, ofSection: section)
+        case let (section, row) where row == sections[section].sessions.count:
+            addSession(to: section)
+        case let (section, row) where row == sections[section].sessions.count + 2 && tableView.isEditing:
+            deleteSection(at: section)
+        case let (section, row):
+            editSession(row, of: section)
         }
     }
 
@@ -139,7 +173,16 @@ class SectionsViewController: UITableViewController {
     override func setEditing(_ editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
 
-        tableView.reloadSections(IndexSet(0..<sections.count), with: .automatic)
+        tableView.reloadSections(IndexSet(0..<sections.count), with: .middle)
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            deleteSection(at: indexPath.section)
+        default:
+            fatalError("Unsupported commit.")
+        }
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -148,7 +191,16 @@ class SectionsViewController: UITableViewController {
             return false
         case (_, 0):
             return false
-        case (let section, let row) where row == sections[section].sessions.count + 1:
+        case (let section, let row) where row > sections[section].sessions.count:
+            return false
+        default:
+            return true
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        switch (indexPath.section, indexPath.row) {
+        case (0..<sections.count, 0):
             return false
         default:
             return true
