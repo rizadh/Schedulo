00308d7769ccf251c2ff5935935dbc90c79b4051
Rizadh Nizam <rizadh@users.noreply.github.com>
2017-07-14 18:30:42 -0400

    Add a dummy 'add' button to the Schedules view

--- Schedulo/SchedulesViewController.swift
+++ Schedulo/SchedulesViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class SchedulesViewController: UITableViewController {
+    // MARK: - Private Properties
+
+    private let stateController: StateController
+
+    // MARK: - Private Methods
+
+    @objc
+    private func addSchedule() {
+        print("Adding schedule")
+    }
+
     // MARK: - Initializers
 
     init(using stateController: StateController) {
-        super.init(style: .grouped)
+        self.stateController = stateController
+
+        super.init(style: .plain)
+
+        let addButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addSchedule))
 
         self.navigationItem.title = "Schedules"
+        self.navigationItem.rightBarButtonItem = addButtonItem
+
         if #available(iOS 11, *) {
             self.navigationItem.largeTitleDisplayMode = .always
         }
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
