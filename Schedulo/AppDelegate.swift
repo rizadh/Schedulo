3afb700228dcc9204843d42a7a6e961ee6530e5e
Rizadh Nizam <me@rizadh.com>
2017-10-11 19:38:49 -0400

    Add temporary icons to the application tab bar

--- Schedulo/AppDelegate.swift
+++ Schedulo/AppDelegate.swift
@@ -15,12 +15,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var stateController: StateController = StateController()
 
+    private let tabBarRects: [CGRect] = [
+        CGRect(x: 10, y: 10, width: 14, height: 14),
+        CGRect(x: 26, y: 10, width: 14, height: 14),
+        CGRect(x: 10, y: 26, width: 14, height: 14),
+        CGRect(x: 26, y: 26, width: 6, height: 6),
+        CGRect(x: 34, y: 26, width: 6, height: 6),
+        CGRect(x: 26, y: 34, width: 6, height: 6),
+        CGRect(x: 34, y: 34, width: 2, height: 2),
+        CGRect(x: 38, y: 34, width: 2, height: 2),
+        CGRect(x: 34, y: 38, width: 2, height: 2),
+        CGRect(x: 38, y: 38, width: 2, height: 2)
+    ]
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let coursesViewController = CoursesViewController()
         coursesViewController.stateController = stateController
+        if #available(iOS 10.0, *) {
+            coursesViewController.tabBarItem.image = UIGraphicsImageRenderer(size: CGSize(width: 50, height: 50)) .image { context in
+                for rect in tabBarRects {
+                    context.fill(rect)
+                }
+            }
+        }
 
         let plansViewController = PlansViewController()
         plansViewController.stateController = stateController
+        if #available(iOS 10.0, *) {
+            plansViewController.tabBarItem.image = UIGraphicsImageRenderer(size: CGSize(width: 50, height: 50)).image { context in
+                for rect in tabBarRects {
+                    context.cgContext.fillEllipse(in: rect)
+                }
+            }
+        }
 
         let navigationControllers = [
             UINavigationController(rootViewController: plansViewController),
